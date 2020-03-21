1a0806dcd33f38df7625ad636d807d92cfa6bb91
RomeoChris <55994561+RomeoChris@users.noreply.github.com>
2020-03-21 19:44:58 +0100

    Add files via upload
    Js code for task 1

---
+++ task1.js
@@ -0,0 +1,9 @@
+var name = "Joseph Unomieta: ";
+var courses = ["Html",  "css",  "javascript "];
+console.log(name + courses);
+
+for (let i=1; i<200; i++) { 
+		if(i % 2 !== 0) { 
+			console.log(i);
+		}
+	}
